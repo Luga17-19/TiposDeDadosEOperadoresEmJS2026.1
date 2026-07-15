1b550734a0a8f7b8ebd75c4a605d6022e9d6ca29
Luga17-19 <lucaspaulogoncalve@gmail.com>
2026-07-14 22:03:56 -0300

    Carga inicial

---
+++ script.js
@@ -0,0 +1,45 @@
+// Vamos dar as Boas-Vindas
+// Uma das formas de se comentar em JS usando//
+
+// Com elas é possivel comentar uma linha simples
+
+// Para comentar mais de uma linha usamos /* */
+
+
+// window.alert("Fala galera! Vamos mandar um Hello World!")
+
+// window.confirm("Tem certeza disso?")
+
+// window.prompt("Qual o seu nome?")
+
+
+// Testando o JS no Console do navegador.
+
+console.log("Tudo certo, o JS está funcionando.!");
+
+// Tipos de DADOS
+
+// 1 - Number
+
+console.log(2); // Para printar numeros no console, não precisamos das aspas ou crase
+console.log(typeof 2);
+console.log(typeof -10);
+console.log(typeof 2.75);
+
+
+// Operacoes aritmeticas
+
+console.log(2 + 5); //7
+console.log(2 - 5); //-3
+console.log(2 * 5); //10
+console.log(2 / 5); //
+console.log(2 * (5 + 3));
+
+
+
+console.log(`aaaa   ${console.log(2 * (5 + 3))}`);
+
+
+
+
+console.log(10 === 10.0)
